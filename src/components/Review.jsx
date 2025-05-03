11aa9e39c92d49373910619a5a223267da5c3a86
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-05-03 02:37:13 -0400

    Started setting up review component

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -1,8 +1,17 @@
+import { useState } from 'react';
+import people from '../data';
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+
 function Review() {
+    const [index, setIndex] = useState(0);
+    const { name, job, image, text } = people[index];
+
     return (
-        <div>
-            Review
-        </div>
+        <article className="review">
+            <div className="img-container">
+                <img src={image} alt={name} className="person-img" />
+            </div>
+        </article>
     )
 }
 
