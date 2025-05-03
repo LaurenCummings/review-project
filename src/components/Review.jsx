b611836b28e10dd4bf0b361c51b769847232e099
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-05-03 02:50:17 -0400

    moved button outside div

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -24,8 +24,8 @@ function Review() {
                 <button className="next-btn">
                     <FaChevronRight />
                 </button>
-                <button className="random-btn">surprise me</button>
             </div>
+            <button className="random-btn">surprise me</button>
         </article>
     )
 }
